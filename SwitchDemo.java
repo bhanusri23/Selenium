04cc83488ec0cfd856df5306cc5fce70a9cde905
Bhanusri <bhanusrichaturvedula@gmail.com>
2016-03-16 15:06:20 +0700

    SwitchDemo.java

---
+++ SwitchDemo.java
@@ -0,0 +1,41 @@
+/* Program to determine the given character is a vowel / consonant */
+
+
+import java.lang.Math; 
+public class ScoreDemo
+{
+  public static void main(String[] args)
+  {
+    char c = 'i';
+    switch(c)
+    {
+    	case 'a':
+      	System.out.println("Vowel is a");
+     	break;
+      
+      case 'e':
+      	System.out.println("Vowel is e");
+     	break;
+      
+      case 'i':
+      	System.out.println("Vowel is i");
+     	break;
+      
+      case 'o':
+      	System.out.println("Vowel is o");
+     	break;
+      
+      case 'u':
+      	System.out.println("Vowel is u");
+     	break;
+      
+      default:
+      		System.out.println("Consonant is : "+c);
+      
+    
+    }
+    
+    }
+  }
+
+
